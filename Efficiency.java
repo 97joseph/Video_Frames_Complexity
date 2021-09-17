bcb3b5d27cc6ec91a29866106d8206f90e93dd36
josephkibira <33089347+97joseph@users.noreply.github.com>
2021-09-17 11:55:34 +0300

    Remove excess logic

---
+++ Efficiency.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+    public static int remove_duplicate_values(int arr[], int size)
+    {
+        int new_size = 1;
+        for(int i=1;i<size;i++)
+        {
+            if(arr[i] != arr[i-1])
+                arr[new_size++] = arr[i];
+        }
+        return new_size;
+    }
+    
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter size of array: ");
+            int size = sc.nextInt();
+            int[] ARRAY = new int[size];
+            System.out.print("Enter the elements of the array: ");
+            for(int i=0;i<size;i++)
+                ARRAY[i] = sc.nextInt();
+            size = remove_duplicate_values(ARRAY,size);
+        for(int i=0;i<size;i++)
+        {
+            System.out.print(ARRAY[i] + " ");
+        }
+        }
+}
